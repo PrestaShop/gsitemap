ac40fa3b2d1905b95eaa2e7ddd4c37406687aedb
jeromenadaud <jerome.nadaud@prestashop.com>
2015-07-07 09:07:58 +0200

    // Version updated to v3.1.0

--- gsitemap.php
+++ gsitemap.php
@@ -37,7 +37,7 @@ class Gsitemap extends Module
 	{
 		$this->name = 'gsitemap';
 		$this->tab = 'seo';
-		$this->version = '3.0.5';
+		$this->version = '3.1.0';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 

cc908a186cc7ee87e6c015226a6864aff9d72b58
gskema <gytis@reservationpartner.lt>
2015-10-22 14:45:43 +0300

    [-] MO: Don't add corrupted links with empty URIs

--- gsitemap.php
+++ gsitemap.php
@@ -330,6 +330,12 @@ class Gsitemap extends Module
 				$url_rewrite = Db::getInstance()->getValue('SELECT url_rewrite, id_shop FROM `'._DB_PREFIX_.'meta_lang` WHERE `id_meta` = '.(int)$meta['id_meta'].' AND `id_shop` ='.(int)$this->context->shop->id.' AND `id_lang` = '.(int)$lang['id_lang']);
 				Dispatcher::getInstance()->addRoute($meta['page'], (isset($url_rewrite) ? $url_rewrite : $meta['page']), $meta['page'], $lang['id_lang']);
 				$uri_path = Dispatcher::getInstance()->createUrl($meta['page'], $lang['id_lang'], array(), (bool)Configuration::get('PS_REWRITING_SETTINGS'));
+
+				// Skip empty URIs (some modules manage to add empty URL rewrites)
+				if (empty($uri_path)) {
+					continue;
+				}
+
 				$url .= Tools::getShopDomainSsl(true).(($this->context->shop->virtual_uri) ? __PS_BASE_URI__.$this->context->shop->virtual_uri : __PS_BASE_URI__).(Language::isMultiLanguageActivated() ? $lang['iso_code'].'/' : '').ltrim($uri_path, '/');
 
 				if (!$this->_addLinkToSitemap(

b3e182e1b3bf5912d6402e8a150dd512a24720b1
Mreker <mathias@reker.dk>
2018-08-08 13:37:53 +0200

    Fixes

--- Readme.md
+++ Readme.md
@@ -33,5 +33,4 @@ That's it: you have contributed to this open-source project! Congratulations!
 
 [1]: http://doc.prestashop.com/display/PS16/Coding+Standards
 [2]: http://doc.prestashop.com/display/PS16/How+to+write+a+commit+message
-[3]: https://help.github.com/articles/using-pull-requests
-
+[3]: https://help.github.com/articles/about-pull-requests/
--- gsitemap-cron.php
+++ gsitemap-cron.php
@@ -1,27 +1,27 @@
 <?php
-
-/*
- *  2007-2016 PrestaShop
- * 
+/**
+ * 2007-2018 PrestaShop
+ *
  * NOTICE OF LICENSE
- * This source file is subject to the Academic Free License (AFL 3.0)
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
  * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/afl-3.0.php* If you did not receive a copy of the license and are unable to
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to license@prestashop.com so we can send you a copy immediately.
- * 
+ *
  * DISCLAIMER
- * 
+ *
  * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
  * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to http://www.prestashop.com for more information.
- * 
- *  @author PrestaShop SA <contact@prestashop.com>
- *  @copyright  2007-2016 PrestaShop SA
- *  @version  Release: $Revision: 7515 $
- *  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
- *  International Registered Trademark & Property of PrestaShop SA
+ * needs please refer to https://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2018 PrestaShop SA
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
  */
 
 /*
@@ -31,27 +31,30 @@
 include(dirname(__FILE__).'/../../config/config.inc.php');
 include(dirname(__FILE__).'/../../init.php');
 /* Check to security tocken */
-if (substr(Tools::encrypt('gsitemap/cron'), 0, 10) != Tools::getValue('token') || !Module::isInstalled('gsitemap'))
-	die('Bad token');
+if ('cli' != php_sapi_name()) {
+    if (Tools::substr(Tools::encrypt('gsitemap/cron'), 0, 10) != Tools::getValue('token') || !Module::isInstalled('gsitemap')) {
+        die('Bad token');
+    }
+}
 
 $gsitemap = Module::getInstanceByName('gsitemap');
 /* Check if the module is enabled */
-if ($gsitemap->active)
-{
-	/* Check if the requested shop exists */
-	$shops = Db::getInstance()->ExecuteS('SELECT id_shop FROM `'._DB_PREFIX_.'shop`');
-	$list_id_shop = array();
-	foreach ($shops as $shop)
-		$list_id_shop[] = (int)$shop['id_shop'];
+if ($gsitemap->active) {
+    /* Check if the requested shop exists */
+    $shops = Db::getInstance()->ExecuteS('SELECT id_shop FROM `'._DB_PREFIX_.'shop`');
+    $list_id_shop = array();
+    foreach ($shops as $shop) {
+        $list_id_shop[] = (int)$shop['id_shop'];
+    }
 
-	$id_shop = (isset($_GET['id_shop']) && in_array($_GET['id_shop'], $list_id_shop)) ? (int)$_GET['id_shop'] : (int)Configuration::get('PS_SHOP_DEFAULT');
-	$gsitemap->cron = true;
-	
-	/* for the main run initiat the sitemap's files name stored in the database */
-	if (!isset($_GET['continue']))
-		$gsitemap->emptySitemap((int)$id_shop);
+    $id_shop = (Tools::getIsset(Tools::getValue('id_shop')) && in_array(Tools::getValue('id_shop'), $list_id_shop)) ? (int)Tools::getValue('id_shop') : (int)Configuration::get('PS_SHOP_DEFAULT');
+    $gsitemap->cron = true;
+    
+    /* for the main run initiat the sitemap's files name stored in the database */
+    if (!Tools::getIsset(Tools::getValue('continue'))) {
+        $gsitemap->emptySitemap((int)$id_shop);
+    }
 
-	/* Create the Google Sitemap's files */
-	$gsitemap->createSitemap((int)$id_shop);
-	
-} 
+    /* Create the Google Sitemap's files */
+    $gsitemap->createSitemap((int)$id_shop);
+}

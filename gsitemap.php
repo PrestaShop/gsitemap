799961f7cfaba3fc46733d23495776ae2161cce1
GoT <PierreRambaud@users.noreply.github.com>
2018-12-05 10:13:43 +0100

    Revert "Use meta description instead of description"

--- gsitemap.php
+++ gsitemap.php
@@ -469,7 +469,7 @@ class Gsitemap extends Module
             if (isset($image_link) && ($file_headers[0] != 'HTTP/1.1 404 Not Found' || $file_headers === true)) {
                 $image_category = array(
                     'title_img' => htmlspecialchars(strip_tags($category->name)),
-                    'caption' => htmlspecialchars(strip_tags($category->meta_description)),
+                    'caption' => Tools::substr(htmlspecialchars(strip_tags($category->description)), 0, 350),
                     'link' => $image_link,
                 );
             }

f4f6dfba1491dcaaf73ff26af607ea7d43854ddf
MathiasReker <mathias@reker.dk>
2018-12-25 12:52:13 +0100

    No whitespace in blank line

--- gsitemap-cron.php
+++ gsitemap-cron.php
@@ -48,15 +48,15 @@ if ($gsitemap->active) {
     foreach ($shops as $shop) {
         $list_id_shop[] = (int) $shop['id_shop'];
     }
-    
+
     $id_shop = (Tools::getIsset(Tools::getValue('id_shop')) && in_array(Tools::getValue('id_shop'), $list_id_shop)) ? (int) Tools::getValue('id_shop') : (int) Configuration::get('PS_SHOP_DEFAULT');
     $gsitemap->cron = true;
-    
+
     /* for the main run initiat the sitemap's files name stored in the database */
     if (!Tools::getIsset(Tools::getValue('continue'))) {
         $gsitemap->emptySitemap((int) $id_shop);
     }
-    
+
     /* Create the Google sitemap's files */
     $gsitemap->createSitemap((int) $id_shop);
 }

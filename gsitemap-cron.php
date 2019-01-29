6cf0e7ae1bc989181fbd1115d6d47e26079229e5
Arthur Deschamps <arthtux@gmail.com>
2019-01-29 16:15:21 -0500

    Change include init.php place

--- gsitemap-cron.php
+++ gsitemap-cron.php
@@ -29,10 +29,11 @@
  */
 
 include(dirname(__FILE__) . '/../../config/config.inc.php');
-include(dirname(__FILE__) . '/../../init.php');
 
 /* Check security token */
 if (!Tools::isPHPCLI()) {
+    include(dirname(__FILE__) . '/../../init.php');
+
     if (Tools::substr(Tools::encrypt('gsitemap/cron'), 0, 10) != Tools::getValue('token') || !Module::isInstalled('gsitemap')) {
         die('Bad token');
     }

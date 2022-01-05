b41ab2ab0890e9ca0f2c1ae96d3184d58038d2f9
sanctusmob <xarilaos.spam@gmail.com>
2022-01-05 10:46:19 +0200

    fix phpstan error v2

--- gsitemap.php
+++ gsitemap.php
@@ -556,7 +556,7 @@ class Gsitemap extends Module
             if ($file_headers[0] != 'HTTP/1.1 404 Not Found' || $file_headers === true) {
                 $manifacturer_image = [
                     'title_img' => htmlspecialchars(strip_tags($manufacturer->name)),
-                    'caption' => Tools::substr(htmlspecialchars(strip_tags($manufacturer->short_description)), 0, 350),
+                    'caption' => Tools::substr(htmlspecialchars(strip_tags($manufacturer->short_description)), 0, 350), /** @phpstan-ignore-line */
                     'link' => $image_link,
                 ];
             }
